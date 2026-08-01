c54b7eb54d408912c6c4099bdb48c724f21b8bee
Vishnu Priyan S T <vishnustvp@gmail.com>
2026-08-01 07:45:06 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[256];
+        if(s.length() != t.length()) return false;
+        for(int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i)]++;
+            arr[t.charAt(i)]--;
+        }
+        for(int val : arr) {
+            if(val != 0) return false;
+        }
+        return true;
+    }
+}
