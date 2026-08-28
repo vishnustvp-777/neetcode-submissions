f3db03b4eff70338d3819b551a46b16f21e30767
Vishnu Priyan S T <vishnustvp@gmail.com>
2026-08-28 16:07:21 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int key = nums[0], count = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(count == 0) {
+                key = nums[i]; 
+            }
+            if(nums[i] == key) count++;
+            else count--;
+        }
+
+        return key;
+    }
+}
